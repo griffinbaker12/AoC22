9df52fef55053f0aac6d44f828ef75cbe9fc5b97
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2023-01-05 11:46:51 -0500

    ..

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AoC22
+
+Little late to the party... but here are my solutions for AoC22.
---
+++ solutionShell.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('./input.txt', 'utf8').trim().split("\n").map(x => x.split(","));
+
+function solution1() {
+    return;
+}
+
+console.log(solution1());
+
+
+function solution2() {
+    return;
+}
+
+console.log(solution2());
