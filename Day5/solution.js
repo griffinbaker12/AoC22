4faeba39b8d96df3ba10a4a28a70cd07b9bea824
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2023-01-05 11:46:31 -0500

    solutionSheel

---
+++ Day5/solution.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('./input.txt', 'utf8').trim().split("\n").map(x => x.split(","));
+
+function solution1() {
+
+}
+
+console.log(solution1());
+
+
+function solution2() {
+
+}
+
+console.log(solution2());
