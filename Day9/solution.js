20b3b584b04da976c02e4d8240a6cf797fb3d37c
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2023-01-08 01:41:08 -0500

    part 2 fix

--- Day9/solution.js
+++ Day9/solution.js
@@ -1,106 +1,106 @@
-const fs = require('fs');
+const { readFileSync } = require("fs");
 
-const data = fs.readFileSync('./input.txt', 'utf8').trim().split("\n").map(x => x.split(" "));
+const lines = readFileSync("input.txt", { encoding: "utf-8" }) // read day??.txt content
+    .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+    .trim() // Remove starting/ending whitespace
+    .split("\n") // Split on newline
+    .map((line) => {
+        const [letter, number] = line.split(" ");
+        return {
+            direction: letter,
+            totalMoves: parseInt(number),
+        };
+    });
 
-// console.log(data);
-
-const moves = {
+const movesDefinition = {
     R: {
         x: 1,
-        y: 0
+        y: 0,
     },
     L: {
         x: -1,
-        y: 0
+        y: 0,
     },
     U: {
         x: 0,
-        y: 1
+        y: -1,
     },
     D: {
         x: 0,
-        y: -1
-    }
-}
+        y: 1,
+    },
+};
 
 class Point {
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
-
-    move(dir) {
-
-        const { x: _x, y: _y } = moves[dir];
-        this.x += _x;
-        this.y += _y;
+    move(direction) {
+        const delta = movesDefinition[direction];
+        this.x += delta.x;
+        this.y += delta.y;
     }
-
-    // this is for diagonal movement
-    follow(x, y, dir) {
-        // move in that same direction, and then have to either move:
-        // L/R if (up / down)
-        // U/D if (left / right)
-        this.move(dir);
-        if (dir === "U" || dir === "D") {
-            if (x > this.x) {
-                this.move("R");
-            } else {
-                this.move("L");
-            }
-        } else {
-            if (y > this.y) {
-                this.move("U");
-            } else {
-                this.move("D");
-            }
+    follow(point) {
+        const distance = Math.max(
+            Math.abs(this.x - point.x),
+            Math.abs(this.y - point.y)
+        );
+        if (distance > 1) {
+            // Move this point
+            const directionX = point.x - this.x;
+            // 0 => do nothing
+            // 1 or 2 => this.x++;
+            // -1 or -2 => this.x--;
+            this.x += Math.abs(directionX) === 2 ? directionX / 2 : directionX;
+            const directionY = point.y - this.y;
+            this.y += Math.abs(directionY) === 2 ? directionY / 2 : directionY;
         }
     }
 }
 
-function solution1() {
+function markVisited(x, y, visited) {
+    visited.add(`${x}-${y}`);
+}
+
+function part1() {
     const head = new Point(0, 0);
     const tail = new Point(0, 0);
+    const visited = new Set();
+    markVisited(0, 0, visited);
 
-    // can we just use `${x}-${y}` and then count the length at the end? think so, let's see
-    const seen = new Set();
-
-    seen.add(`${tail.x}-${tail.y}`);
-
-    for (const [direction, distance] of data) {
-        // console.log(direction, 'dir is');
-        for (let i = 1; i <= Number(distance); i++) {
-            head.move(direction);
-            // move diag
-            if ((Math.abs(head.x - tail.x) !== 0 && Math.abs(head.y - tail.y) > 7) ||
-                (Math.abs(head.y - tail.y) !== 0 && Math.abs(head.x - tail.x) > 7)
-            ) {
-                // console.log('FOLLOW', head, tail);
-                tail.follow(head.x, head.y, direction);
-                if (!seen.has(`${tail.x}-${tail.y}`)) {
-                    seen.add(`${tail.x}-${tail.y}`);
-                }
-            } else if ((Math.abs(head.x - tail.x) == 7) || (Math.abs(head.y - tail.y) == 7)) {
-                // console.log('MOVE');
-                tail.move(direction);
-                if (!seen.has(`${tail.x}-${tail.y}`)) {
-                    seen.add(`${tail.x}-${tail.y}`);
-                }
-            }
-
-            // console.log('SEEN IN', seen, "TAIL", tail);
+    for (const line of lines) {
+        for (let i = 0; i < line.totalMoves; i++) {
+            head.move(line.direction);
+            tail.follow(head);
+            markVisited(tail.x, tail.y, visited);
         }
     }
 
-    // console.log(seen);
-
-    return seen.size;
+    console.log(visited.size);
 }
 
-console.log(solution1());
+function part2() {
+    const knots = new Array(10).fill(0).map((_) => new Point(0, 0));
+    const visited = new Set();
+    markVisited(0, 0, visited);
 
-function solution2() {
-    return;
+    for (const line of lines) {
+        for (let i = 0; i < line.totalMoves; i++) {
+            // Move the head according to the instructions
+            knots[0].move(line.direction);
+            // Move the rest of the rope
+            for (let knot = 1; knot < knots.length; knot++) {
+                const point = knots[knot];
+                point.follow(knots[knot - 1]);
+            }
+            const tail = knots[knots.length - 1];
+            markVisited(tail.x, tail.y, visited);
+        }
+    }
+
+    console.log(visited.size);
 }
 
-console.log(solution2());
+part1();
+part2();
