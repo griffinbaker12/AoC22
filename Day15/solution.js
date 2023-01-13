55dad95edbbe8fc4221d7c180951de1b30930180
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2023-01-13 14:57:54 -0500

    part 1 :)

---
+++ Day15/solution.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('./input.txt', 'utf8').trim().split("\n").map(x => x.split(","));
+
+function solution1() {
+    return;
+}
+
+console.log(solution1());
+
+function solution2() {
+    return;
+}
+
+console.log(solution2());
